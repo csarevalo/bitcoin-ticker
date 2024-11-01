13978ab2a34db5b84cfa86d3ac142bf0294512d0
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-31 21:46:24 -0700

    created network helper, and currency provider... still in progress

---
+++ lib/src/services/network_helper.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+const String _apiKey = '466291F6-86AD-4ADE-8DAD-39B743022341';
+
+class NetworkHelper {
+  final String baseURL;
+  final String version;
+  final String request;
+
+  NetworkHelper({
+    required this.baseURL,
+    required this.version,
+    required this.request,
+  });
+
+  /// Get the current exchange rate between requested asset and all other assets.
+  Future<dynamic> getAllCurrentRates(
+    /// Requested exchange rates base asset identifier
+    String assetIdBase,
+  ) async {
+    String url = '$baseURL/$version/$request/$assetIdBase?apiKey=$_apiKey';
+
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      debugPrint(response.statusCode.toString());
+    }
+    return null;
+  }
+
+  /// Retrieves the exchange rate for a specific base and quote asset at a given time or the current rate.
+  Future<dynamic> getSpecificCurrentRate(
+    /// Requested exchange rates base asset identifier
+    String assetIdBase,
+
+    /// Requested exchange rate quote asset identifier
+    String assetIdQuote,
+  ) async {
+    String url =
+        '$baseURL/$version/$request/$assetIdBase/$assetIdQuote?apiKey=$_apiKey';
+
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      debugPrint(response.statusCode.toString());
+    }
+    return null;
+  }
+}
